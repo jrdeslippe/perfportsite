7af3d9cd00d0c88cb6e3930e8678dacff9dc7ac3
Jack Deslippe <jdeslip@gmail.com>
2017-08-15 15:49:12 -0800

    Deployed 5053027 with MkDocs version: 0.16.3

--- facilities/titan_script.sh
+++ facilities/titan_script.sh
@@ -3,13 +3,13 @@
 #PBS -N test
 #PBS -j oe
 export n_nodes=$JOBSIZE
-export n_mpi_ranks_per_node=32
+export n_mpi_ranks_per_node=8
 export n_mpi_ranks=$(($n_nodes * $n_mpi_ranks_per_node))
 
 cd $MEMBERWORK/<yourOLCFProjectName>
 date
 
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=2
 
 aprun -n $n_mpi_ranks -N $n_mpi_ranks_per_node \
-  -d 16  <executable> <executable args>
+  -d 2  <executable> <executable args>
